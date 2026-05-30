6144c3dcc9af2b15efdd543995bd77595c315f39
dtecindustries <joachim.defour@icloud.com>
2026-05-30 12:09:18 +0200

    add stream configuration for width and height

--- services/vision/config.py
+++ services/vision/config.py
@@ -8,3 +8,5 @@ CAMERA_URL = os.getenv("CAMERA_URL", "")  # set this to use a remote MJPEG strea
 STREAM_HOST = os.getenv("STREAM_HOST", "0.0.0.0")
 STREAM_PORT = int(os.getenv("STREAM_PORT", 8000))
 STREAM_FPS = int(os.getenv("STREAM_FPS", 30))
+STREAM_WIDTH = int(os.getenv("STREAM_WIDTH", 640))
+STREAM_HEIGHT = int(os.getenv("STREAM_HEIGHT", 480))
